75f5a5959d0e6f42978ce313254a5ed34fb4f077
Nicolau Barsotti <nicolau.barsotti@flexcodellc.com>
2021-12-04 17:52:59 -0300

    feat: create Item and InventoryProps interfaces, tweak Inventory component a bit

--- src/components/inventory/Inventory.tsx
+++ src/components/inventory/Inventory.tsx
@@ -1,14 +1,23 @@
 import { Inventory, InventoryContent, ItemWrapper } from './styles';
 
-export default () => {
-  const mockItem = 'data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAACQAAAAgCAYAAAB6kdqOAAABvUlEQVR4Xu2XPW7DMAyF7UtkyRIgQ4AAAYwAQaZOnbJ16NKhQ4fe/whqaeJVj5RkO7biLH0Ah0i0+Zk/stM0/6qmNgzbKooBd9utsffTMdwOh/C634eX3S4DWB0yhWD76E7h63IOn+euh3s7WsDrr09FqLYP4CHEfBYABjj5LYDw7frsLZYCeSgEYGM4gH1fL+4BFqk1JeCsRAj1Y/Nw/iFsjLukQOgHBuGg+VI2DXxyezOkPSFAHqYEAkubuAqUAkl2MMq5J84ZX+Phpfxxb7Lafkr44qkwYtLMcq1kNmar/QO195wkzQ7fcAyIs4Gx5z2GmQXEN+PeKUH4Zmc/DAWf5NF3klIgrEuTc39YOPXhdYZJh2OycpnQdZQDpfTZ4usAlDs2bLxRpUCcJZzCPnjJBGgBDJRC8d49QBVgRAqEs2jssPOGElaCgTQTgLKl0/2SAaIiDBQbWQ47myV9X5WM/SorQmFavMcTFKer22xciaxfuvYw5Uee4fID8FANj7wMwMw3+hINjzy+p1fuNzvmHoo/fVfrKzviGlSyU/o7tAoUC2D49PVZXOF8ykmBGGClw3JIT8tGWTVgfgDysYBPjVasdQAAAABJRU5ErkJggg==';
+export interface Item { 
+  id: number,
+  name: string,
+  examine: string,
+  url: string,
+  image: string // base64 string
+}
+export interface InventoryProps {
+  items: Item[]
+}
 
+export default ({ items }: InventoryProps) => {
   return (
     <Inventory>
       <InventoryContent>
-        {Array.from(Array(27).keys()).map((i) => (
-          <ItemWrapper key={i}>
-            <img src={mockItem} />
+        {items.map(({id, image }) => (
+          <ItemWrapper key={id}>
+            <img src={`data:image/png;base64, ${image}`} />
           </ItemWrapper>
         ) ) }
       </InventoryContent>
