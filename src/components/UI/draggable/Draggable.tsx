d9cccbc62664b96eb52ab2ab3a113ee74f748927
Nicolau Barsotti <nicolau.barsotti@flexcodellc.com>
2021-12-10 11:33:15 -0300

    feat: integrate Draggable with PositionContext

--- src/components/UI/draggable/Draggable.tsx
+++ src/components/UI/draggable/Draggable.tsx
@@ -1,26 +1,47 @@
 import { useEffect, useState } from 'react';
 import { Rnd } from 'react-rnd';
 import { DraggableProps } from './interfaces';
+import { usePosition, PositionComponents } from '../../../context/PositionContext';
 
-export default ({ children, startPosition }: DraggableProps ): JSX.Element => {
+export default ({ children, startPosition, component }: DraggableProps ): JSX.Element => {
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
+  const { setWidth, setHeight, setTop, components } = usePosition();
+  
+  const currentComponent = components[component];
+  const { onTop } = currentComponent; 
+
+  const setTopHandler = () => {
+    if (onTop) return;
+    
+    setTop(component, true);
+
+    for (const comp in PositionComponents) {
+      const selectedComponent = PositionComponents[comp as keyof typeof PositionComponents];
+      if (selectedComponent !== component) {
+        setTop(selectedComponent, false);
+      }
+    }
+  };
 
   const setPositionHandler = (x: number, y: number) => { 
     setX(x);
     setY(y);
+    setWidth(x, component);
+    setHeight(y, component);
   };
 
   useEffect(() => { 
     if (startPosition) { 
-      const {x, y } = startPosition;
-      setX(x);
-      setY(y);
+      const { x, y } = startPosition;
+      setPositionHandler(x, y);
     }
   }, [startPosition]);
 
   return (
     <Rnd
+      style={{ zIndex: onTop ? 999 : 0 }}
+      onDragStart={setTopHandler}
       onDragStop={(e, d) => setPositionHandler(d.x, d.y)}
       enableResizing={false}
       bounds="parent"
--- src/components/UI/draggable/interfaces.tsx
+++ src/components/UI/draggable/interfaces.tsx
@@ -1,5 +1,8 @@
+import { PositionComponents } from "../../../context/PositionContext";
+
 export interface DraggableProps {
   children: JSX.Element;
+  component: PositionComponents;
   startPosition?: { 
     x: number;
     y: number;
