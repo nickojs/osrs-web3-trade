a377b599b2c2d6792cf98ca6f120b88390e23277
Nicolau Barsotti <nicolau.barsotti@flexcodellc.com>
2021-12-10 01:38:08 -0300

    refactor: adjust Main hierarchy and logic

--- src/Containers/Main.tsx
+++ src/Containers/Main.tsx
@@ -1,34 +1,23 @@
-import { Rnd } from 'react-rnd';
 import MakertPlace from '../components/marketplace/MarketPlace';
 import Inventory from '../components/inventory/Inventory';
+import Draggable from '../components/UI/draggable/Draggable';
 import { Container } from './styles';
+import useResize from '../hooks/useResize';
+import positionHandler, { Position } from '../helpers/positionHandler';
 
-interface DraggableProps {
-  children: JSX.Element;
-  startPosition?: { 
-    x: number;
-    y: number;
-  }
-}
-
-const Draggable = ({ children, startPosition }: DraggableProps ): JSX.Element => (
-  <Rnd
-    enableResizing={false}
-    bounds="parent"
-    position={startPosition}
-  >
-    {children}
-  </Rnd>
-);
 
 export default (): JSX.Element => { 
+  const { width, height } = useResize();
+  const marketPlacePos = positionHandler(Position.CENTER, { width: 600, height: 400 }, { width, height });
+  const inventoryPos = positionHandler(Position.BOTTOMRIGHT, { width: 382, height: 532 }, { width, height });
+
   return (
     <Container>
-      <Draggable>
+      <Draggable startPosition={marketPlacePos}>
         <MakertPlace />
       </Draggable>
 
-      <Draggable>
+      <Draggable startPosition={inventoryPos}>
         <Inventory items={[]}/>
       </Draggable>
     </Container>
