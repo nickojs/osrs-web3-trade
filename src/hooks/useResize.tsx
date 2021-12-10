477276248ce667341a64aeda8f71fb0096f74b44
Nicolau Barsotti <nicolau.barsotti@flexcodellc.com>
2021-12-10 01:02:16 -0300

    feat: create useResize hook to handle screen positioning

---
+++ src/hooks/useResize.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect, useCallback } from 'react';
+
+export default (): { width: number, height: number } => { 
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  const handleWindowResize = useCallback(() => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  },[]);
+
+  useEffect(() => { 
+    // get initial sizing
+    handleWindowResize();
+    
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => window.removeEventListener('resize', handleWindowResize);
+  }, []);
+
+  return { width, height };
+};
