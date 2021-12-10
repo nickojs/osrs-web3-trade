5e24c7bf78e05c39447dfc75c1deac11512f8a60
Nicolau Barsotti <nicolau.barsotti@flexcodellc.com>
2021-12-10 01:34:51 -0300

    feat: extend Draggable component

--- src/components/UI/draggable/Draggable.tsx
+++ src/components/UI/draggable/Draggable.tsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from 'react';
 import { Rnd } from 'react-rnd';
 import { DraggableProps } from './interfaces';
 
-export default ({ children, startPosition }: DraggableProps ): JSX.Element => (
-  <Rnd
-    enableResizing={false}
-    bounds="parent"
-    position={startPosition}
-  >
-    {children}
-  </Rnd>
-);
+export default ({ children, startPosition }: DraggableProps ): JSX.Element => {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  const setPositionHandler = (x: number, y: number) => { 
+    setX(x);
+    setY(y);
+  };
+
+  useEffect(() => { 
+    if (startPosition) { 
+      const {x, y } = startPosition;
+      setX(x);
+      setY(y);
+    }
+  }, [startPosition]);
+
+  return (
+    <Rnd
+      onDragStop={(e, d) => setPositionHandler(d.x, d.y)}
+      enableResizing={false}
+      bounds="parent"
+      position={{ x, y }}
+    >
+      {children}
+    </Rnd>
+  );
+};
