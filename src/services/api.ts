c7bd3173f1da8bf4ef3ed0a30332e76ecb7836a9
Nicolau Barsotti <nbarsotti94@gmail.com>
2022-06-29 00:47:28 -0300

    fix: adjust api baseUrl

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 const determineUrl = () => {
   switch (process.env.NODE_ENV) {
-  case 'development':
-    return 'http://localhost:3000';
-  case 'production':
-    return 'https://nickojs.herokuapp.com';
-  default:
-    throw new Error('Unkonwn NODE_ENV value provided');
+    case 'development':
+      return 'http://localhost:7000';
+    case 'production':
+      return 'https://nickojs.herokuapp.com';
+    default:
+      throw new Error('Unkonwn NODE_ENV value provided');
   }
 };
 
