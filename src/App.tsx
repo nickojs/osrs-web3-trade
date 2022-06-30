e025e9d8eb24c7eea09b548d2a38b4c9425bc058
Nicolau Barsotti <nbarsotti94@gmail.com>
2022-06-30 13:37:31 -0300

    feat: integrate react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { PositionProvider } from './context/PositionContext';
 import { ToastProvider } from './context/NotificationContext';
 import { GlobalStyle } from './globalStyles';
 import Notification from './components/notification/Notification';
 import routes from './routes';
 
+const queryClient = new QueryClient();
+
 export default () => (
-  <ToastProvider>
-    <PositionProvider>
-      <GlobalStyle />
-      <Notification />
-      {routes}
-    </PositionProvider>
-  </ToastProvider>
+  <QueryClientProvider client={queryClient}>
+    <ToastProvider>
+      <PositionProvider>
+        <GlobalStyle />
+        <Notification />
+        {routes}
+      </PositionProvider>
+    </ToastProvider>
+  </QueryClientProvider>
 );
