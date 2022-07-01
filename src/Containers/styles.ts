29174a690bdf55c9be1043185ec49990f1f62ebd
Nicolau Barsotti <nbarsotti94@gmail.com>
2022-07-01 01:49:49 -0300

    feat: add background to container

--- src/Containers/styles.ts
+++ src/Containers/styles.ts
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
+import bg from '../assets/bg_texture.jpg';
 
 export const Container = styled.section`
   position: relative;
   width: 100vw;
   height: 100vh;
+`;
+
+export const LoginContainer = styled(Container)`
+  background-image: url(${bg});
+  background-repeat: repeat;
+  display: grid;
+  align-items: center;
+  justify-content: center;
 `;
