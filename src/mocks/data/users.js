8f1cef57f6da25425bcab5931f24840324ba6945
Nicolau Barsotti <nbarsotti94@gmail.com>
2022-07-01 00:55:33 -0300

    fix: add a more realistic userId value

--- src/mocks/data/users.js
+++ src/mocks/data/users.js
@@ -6,31 +6,31 @@ import mockedFace5 from './assets/user/mockedFace5.jpg';
 
 export default [
   {
-    userId: 'warrior-id',
+    userId: '8babc1e9-60cb-40c1-bde1-a69374dce398',
     username: 'warrior',
     picture: mockedFace1,
     status: 'online'
   },
   {
-    userId: 'another-random-id',
+    userId: '572d0a91-b4a0-4978-a9b3-fb6539bb5293',
     username: 'magic wizard',
     picture: mockedFace2,
     status: 'away'
   },
   {
-    userId: 'YetANOTHERandomId',
+    userId: '01d9fa59-4f32-48ef-8f19-5bb84a04f040',
     username: 'party peter',
     picture: mockedFace3,
     status: 'away'
   },
   {
-    userId: 'rrrrr',
+    userId: 'f9b93408-43c5-4a00-a3b2-3b5bc908ec0d',
     username: 'another warrior',
     picture: mockedFace4,
     status: 'busy'
   },
   {
-    userId: 'pppppppppp',
+    userId: 'b902cbd3-196d-41f1-aabf-aa0d04e9b750',
     username: 'hans',
     picture: mockedFace5,
     status: 'online'
