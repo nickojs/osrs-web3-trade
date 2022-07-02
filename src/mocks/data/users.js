a75e879411edc6bf184dcbabffdd9420c6aea5c0
Nicolau Barsotti <nbarsotti94@gmail.com>
2022-07-02 01:10:56 -0300

    fix: adjust mocks

--- src/mocks/data/users.js
+++ src/mocks/data/users.js
@@ -1,38 +1,32 @@
-import mockedFace1 from './assets/user/mockedFace1.jpg';
-import mockedFace2 from './assets/user/mockedFace2.jpg';
-import mockedFace3 from './assets/user/mockedFace3.jpg';
-import mockedFace4 from './assets/user/mockedFace4.jpg';
-import mockedFace5 from './assets/user/mockedFace5.jpg';
-
 export default [
   {
-    userId: '8babc1e9-60cb-40c1-bde1-a69374dce398',
+    id: '8babc1e9-60cb-40c1-bde1-a69374dce398',
     username: 'warrior',
-    picture: mockedFace1,
+    profilePicId: 0,
     status: 'online'
   },
   {
-    userId: '572d0a91-b4a0-4978-a9b3-fb6539bb5293',
+    id: '572d0a91-b4a0-4978-a9b3-fb6539bb5293',
     username: 'magic wizard',
-    picture: mockedFace2,
+    profilePicId: 1,
     status: 'away'
   },
   {
-    userId: '01d9fa59-4f32-48ef-8f19-5bb84a04f040',
+    id: '01d9fa59-4f32-48ef-8f19-5bb84a04f040',
     username: 'party peter',
-    picture: mockedFace3,
+    profilePicId: 2,
     status: 'away'
   },
   {
-    userId: 'f9b93408-43c5-4a00-a3b2-3b5bc908ec0d',
+    id: 'f9b93408-43c5-4a00-a3b2-3b5bc908ec0d',
     username: 'another warrior',
-    picture: mockedFace4,
+    profilePicId: 3,
     status: 'busy'
   },
   {
-    userId: 'b902cbd3-196d-41f1-aabf-aa0d04e9b750',
+    id: 'b902cbd3-196d-41f1-aabf-aa0d04e9b750',
     username: 'hans',
-    picture: mockedFace5,
+    profilePicId: 4,
     status: 'online'
   }
 ];
