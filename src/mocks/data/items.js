3558b20352ca7a7ed87cd959beb340c95dd0039c
Nicolau Barsotti <nbarsotti94@gmail.com>
2022-07-09 20:40:58 -0300

    refactor: use local assets instead of problematic API url

--- src/mocks/data/items.js
+++ src/mocks/data/items.js
@@ -1,25 +1,33 @@
+import item45321 from './assets/45321.gif';
+import item45328 from './assets/45328.gif';
+import item45335 from './assets/45335.gif';
+import item4151 from './assets/4151.gif';
+
+// Runescape API keeps changing the assets URL,
+// that's why I decided to kept the icons locally for the mocks
+
 export default [
 {
   id: 45321,
-  icon: 'https://secure.runescape.com/m=itemdb_rs/1656325890284_obj_sprite.gif?id=45321',
+  icon: item45321,
   description: 'Banite tuned against abyssals, so an abyssalite two handed sword?',
   name: 'Abyssal bane 2h sword'
 },
 {
   id: 45328,
-  icon: 'https://secure.runescape.com/m=itemdb_rs/1656325890284_obj_sprite.gif?id=45328',
+  icon: item45328,
   description: 'Banite tuned against abyssals, so an abyssalite longsword?',
   name: 'Abyssal bane longsword'
 },
 {
   id: 45335,
-  icon: 'https://secure.runescape.com/m=itemdb_rs/1656325890284_obj_sprite.gif?id=45335',
+  icon: item45335,
   description: 'Banite tuned against abyssals, so an abyssalite off hand longsword?',
   name: 'Abyssal bane off-hand longsword'
 },
 {
   id: 4151,
-  icon: 'https://secure.runescape.com/m=itemdb_rs/1656325890284_obj_sprite.gif?id=4151',
+  icon: item4151,
   description: 'A weapon from the Abyss.',
   name: 'Abyssal whip'
 }
